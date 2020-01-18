e92327db017fe1333f935769d612ab84313a4b59
“Carmont <carmontconnect@gmail.com>
2020-01-18 21:13:31 +0000

    any product should work

--- content.js
+++ content.js
@@ -35,6 +35,9 @@ let carbonFootprintPerUSD = {
 'Car':  0.0000184294 ,
 'Train':  0.0000036  ,
 'Taxi':  0.0001001  ,
+'zero':0,
+'digitalMusic':0,
+
 };
 
 
@@ -49,29 +52,202 @@ function alertCat() {
     let cat = document.getElementById('nav-subnav').getAttribute('data-category').trim();
     // alert(cat);
 
+    let priceTags = [['id','priceblock_ourprice'],
+                        ['class','offer-price'],
+                        ['class','a-color-price']
+                    
+                    ]
+
+    let priceStr = 0;
+    let pricesFound = [] 
+    priceTags.forEach(([attributeType,attributeName]) => {
+
+
+        if(attributeType == 'id'){
+
+            if(document.getElementById(attributeName)){
+                priceStr =  document.getElementById(attributeName).innerText.trim();
+                pricesFound.push([attributeType,attributeName])
+            }
+                
+        }
+        else if(attributeType == 'class'){
+
+            if(document.getElementsByClassName(attributeName).length!==0){
+                priceStr =  document.getElementsByClassName(attributeName)[0].innerText.trim();
+                pricesFound.push([attributeType,attributeName])
+            }
+
+        }
+
+    })
+
+    alert(priceStr);
+
+    // let priceStr = document.getElementById('priceblock_ourprice') ? document.getElementById('priceblock_ourprice').innerText.trim() :null ;
     
-    let priceStr = document.getElementById('priceblock_ourprice') ? document.getElementById('priceblock_ourprice').innerText.trim() :null ;
-    
-    if(!priceStr){
-        priceStr = document.getElementsByClassName('offer-price') ? document.getElementsByClassName('offer-price')[0].innerText.trim() : null ;
-    }
-    else if(!priceStr){
-        priceStr = document.getElementsByClassName('a-color-price') ? document.getElementsByClassName('a-color-price')[0].innerText.trim() : null ;
+    // if(!priceStr){
+    //     priceStr = document.getElementsByClassName('offer-price') ? document.getElementsByClassName('offer-price')[0].innerText.trim() : null ;
+    // }
+    // else if(!priceStr){
+    //     priceStr = document.getElementsByClassName('a-color-price') ? document.getElementsByClassName('a-color-price')[0].innerText.trim() : null ;
         
-    }
+    // }
     let priceNumber = Number(priceStr.substr(1));
     let costPerMetricTon = 5; 
 
+    
+
     // alert(cat)
     let offsetAmount = 5;
-    if(cat ==="computers"){
-        offsetAmount = costPerMetricTon*priceNumber*carbonFootprintPerUSD['ComputersITEquipment'];
-    } else if (cat ==="dvd"){
-        offsetAmount = costPerMetricTon*priceNumber*carbonFootprintPerUSD['TelevisionRadioPhoneEquipment'];
-    }
-   
+    let emissionsCategory = 'ComputersITEquipment'
+    alert(cat);
+    switch(cat){
+        case("computers"):
+            emissionsCategory = 'ComputersITEquipment'
+            break;
+        case("dvd"):
+            emissionsCategory= 'TelevisionRadioPhoneEquipment'
+            break;
+        case("drugstore"):
+            emissionsCategory= 'Pharmaceuticals'
+            break;
+        case('kitchen'):
+            emissionsCategory= 'FurnitureAndOtherManufacturedGoods'
+            break;
+        case('pantry'):
+            emissionsCategory= 'MediumMeatEater'
+            break;
+        case('baby'):
+            emissionsCategory= 'ClothesTextilesShoes'
+            break;
+        case('beauty'):
+            emissionsCategory= 'Pharmaceuticals'
+            break;
+        case('books'):
+            emissionsCategory= 'PaperProducts'
+            break;
+        case('automotive'):
+            emissionsCategory= 'MotorVehiclesWithoutFuel'
+            break;
+        case('music'):
+            emissionsCategory= 'zero'
+            break;
+        case('clothing'):
+            emissionsCategory= 'ClothesTextilesShoes'
+            break;
+        case('diy'):
+            emissionsCategory= 'FurnitureAndOtherManufacturedGoods'
+            break;
+        case('electronics'):
+            emissionsCategory= 'ComputersITEquipment'
+            break;
+        case('shoes'):
+            emissionsCategory= 'ClothesTextilesShoes'
+            break;
+        case('outdoors'):
+            emissionsCategory= 'FurnitureAndOtherManufacturedGoods'
+            break;
+        case('grocery'):
+            emissionsCategory= 'MediumMeatEater'
+            break;
+        case('handmade'):
+            emissionsCategory= 'ClothesTextilesShoes'
+            break;
+        case('handmade'):
+            emissionsCategory= 'ClothesTextilesShoes'
+            break;
+        case('industrial'):
+            emissionsCategory= 'ComputersITEquipment'
+            break;
+        case('tools'):
+            emissionsCategory= 'ComputersITEquipment'
+            break;
+        case('jewelry'):
+            emissionsCategory= 'FurnitureAndOtherManufacturedGoods'
+            break;
+        case('digital-text'):
+            emissionsCategory= 'zero'
+            break;
+        case('appliances'):
+            emissionsCategory= 'FurnitureAndOtherManufacturedGoods'
+            break;
+        case('lighting'):
+            emissionsCategory= 'ComputersITEquipment'
+            break;
+        case('luggage'):
+            emissionsCategory= 'ClothesTextilesShoes'
+            break;
+        case('luxury-beauty'):
+            emissionsCategory= 'Pharmaceuticals'
+            break;
+        case('musical-instruments'):
+            emissionsCategory= 'ComputersITEquipment'
+            break;
+        case('videogames'):
+            emissionsCategory= 'ComputersITEquipment'
+            break;
+        case('pet-supplies'):
+            emissionsCategory= 'MediumMeatEater'
+            break;
+        case('instant-video'):
+            emissionsCategory= 'zero'
+            break;
+        case('software'):
+            emissionsCategory= 'zero'
+            break;
+        case('sports'):
+            emissionsCategory= 'FurnitureAndOtherManufacturedGoods'
+            break;
+        case('officeproduct'):
+            emissionsCategory= 'PaperProducts'
+            break;
+        case('toys'):
+            emissionsCategory= 'FurnitureAndOtherManufacturedGoods'
+            break;
+        case('video'):
+            emissionsCategory= 'TelevisionRadioPhoneEquipment'
+            break;
+        case('watch'):
+            emissionsCategory= 'FurnitureAndOtherManufacturedGoods'
+            break;
+           
+        
+        default:
+            break;
 
-    document.getElementsByClassName('offer-price')[0].parentElement.innerHTML+= '<div style="text-decoration:none !important;">  --> + £'+ offsetAmount.toFixed(2).toString() + " offset </div>"
+
+
+    }
+
+
+    
+    offsetAmount = costPerMetricTon*priceNumber*carbonFootprintPerUSD[emissionsCategory];
+    // alert(offsetAmount);
+   
+    if(pricesFound.length!==0){
+        pricesFound.forEach((attr) =>{
+            let attributeType = attr[0];
+            let attributeName = attr[1];
+
+
+            if(attributeType == 'class'){
+
+                document.getElementsByClassName(attributeName)[0].parentElement.innerHTML+= '<div style="text-decoration:none !important;">  --> + £'+ offsetAmount.toFixed(2).toString() + " offset </div>"
+
+             
+            }
+            else if(attributeType == 'id'){
+
+                document.getElementById(attributeName).parentElement.innerHTML+= '<div style="text-decoration:none !important;">  --> + £'+ offsetAmount.toFixed(2).toString() + " offset </div>"
+
+
+            }
+
+        })
+    }
+    
+   
 
 }
 
